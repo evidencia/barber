f3291d1cc74eb310462480d20c40af3f6cc97415
evidencia <evidenciadavid2016@gmail.com>
2024-03-12 19:44:30 +0000

    feat: add authProvider

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,16 +1,21 @@
+
 import React from 'react'
-import { Card } from './ui/card'
+import { Card, CardContent } from './ui/card'
 import Image from 'next/image'
 import { Button } from './ui/button'
 import { Menu } from 'lucide-react'
 
 export default function Header() {
+
+
   return (
-    <Card className='flex justify-between items-center flex-row p-5 '>
+    <Card >
+     <CardContent className='flex justify-between items-center flex-row p-5'>
       <Image src="/logo.png" alt='FSW Barber' width={120} height={22} />
-      <Button variant="outline" size="icon" className='w-8 h-8'>
-        <Menu size={18} />
-      </Button>
+        <Button variant="outline" size="icon" className='w-8 h-8'>
+          <Menu size={18} />
+        </Button>
+     </CardContent>
     </Card>
   )
 }
